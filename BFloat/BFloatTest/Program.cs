a236aa8ec8d12209ea1dff2f3c676da5903a412c
Michael Chen <michaelgx2@live.cn>
2019-12-13 22:54:45 +0800

    Added test project. Added new ToString method.

---
+++ BFloat/BFloatTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bfloat = BFloat;
+
+namespace BFloatTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bfloat b1 = 152300;
+            //bfloat b2 = new BFloat(1.2411, 26+15);
+            //bfloat b3 = 23523423423423234234568.0;
+            //Console.WriteLine(
+            //    $"{b1.ToString(BFloatStringFormat.UseLetters)}\n{b2.ToString(BFloatStringFormat.UseLetters)}\n{b3.ToString(BFloatStringFormat.UseLetters)}");
+            bfloat b = 1.523;
+            for (int i = 0; i < 1000; i++)
+            {
+                b *= 10;
+                //Console.WriteLine(b.ToString(BFloatStringFormat.UseLetters));
+                Console.WriteLine(b.ToString(BFloatStringFormat.ChineseAndLetters));
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
